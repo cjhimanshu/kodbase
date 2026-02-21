b8b44803eeafe2fa0b2e720188bc3b9f5f6c3d2b
cjhimanshu <cjhimanshu49@gmail.com>
2026-02-21 16:10:04 +0530

    chore: remove accidentally nested folder kb

--- kb/kodebase-project-main/kodebase-project-main/backend/utils/errors/EmailServiceError.js
+++
@@ -1,9 +0,0 @@
-class EmailServiceError extends Error {
-  constructor(message, statusCode = 500) {
-    super(message);
-    this.name = 'EmailServiceError';
-    this.statusCode = statusCode;
-  }
-}
-
-module.exports = EmailServiceError;
